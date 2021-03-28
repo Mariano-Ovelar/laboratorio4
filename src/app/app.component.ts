dc87d2891101b839dfc236307972f69077b61981
Mariano <marianoovelar200@gmail.com>
2021-03-28 05:27:53 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mis-proyectos';
+  
+  edadUno: number = 0;
+  edadDos: number = 0;
+  suma: number = 0;
+  promedio: number = 0;
+
+
+  Promediar() {
+    this.promedio = this.suma / 2;
+  }
+  Sumar() {
+    this.suma = this.edadUno + this.edadDos;
+  }
+  Calcular() {
+    this.Sumar();
+    this.Promediar();
+  }
+  Limpiar(){
+    this.edadUno=0;
+    this.edadDos=0;
+    this.suma=0;
+    this.promedio=0;
+  }
 }
